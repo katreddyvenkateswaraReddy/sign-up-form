4375fbaf52a761f8691aa6e68f00fc0691caff83
katreddyvenkateswaraReddy <katreddy41@gmail.com>
2023-12-16 20:57:15 +0530

    New Changes

---
+++ src/Components/SignUp.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
+  const [confirmPasswordValid, setConfirmPasswordValid] = useState(true);
+
+  const [emailBorderColor, setEmailBorderColor] = useState("");
+  const [passwordBorderColor, setPasswordBorderColor] = useState("");
+  const [confirmPasswordBorderColor, setConfirmPasswordBorderColor] = useState("");
+
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+    setEmailValid(/^\S+@\S+\.\S+$/.test(value) && value.trim() !== "");
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    setPasswordValid(value.length >= 8 && value.trim() !== "");
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    const value = e.target.value;
+    setConfirmPassword(value);
+    setConfirmPasswordValid(value === password && value.trim() !== "");
+  };
+
+  const handleInputFocus = (setState) => () => {
+    // Set the border color to blue when the input is focused
+    setState("blue");
+  };
+
+  const handleInputBlur = (setState, isValid) => () => {
+    // Set the border color based on validation status
+    setState(isValid ? "valid" : "invalid");
+  };
+
+  const handleInputChange = (setState) => () => {
+    // Set the border color to blue when typing
+    setState("blue");
+  };
+
+  const handleSubmit = () => {
+    if (email.trim() === "" || password.trim() === "" || confirmPassword.trim() === "") {
+      alert("Form cannot be submitted. Please fill in all fields.");
+      // Set border color to red for empty fields
+      setEmailBorderColor(email.trim() === "" ? "invalid error" : "input");
+      setPasswordBorderColor(password.trim() === "" ? "invalid error" : "input");
+      setConfirmPasswordBorderColor(confirmPassword.trim() === "" ? "invalid error" : "input");
+      return;
+    }
+
+    if (emailValid && passwordValid && confirmPasswordValid) {
+      alert("Form submitted successfully");
+      // Reset the form
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      // Reset border colors
+      setEmailBorderColor("");
+      setPasswordBorderColor("");
+      setConfirmPasswordBorderColor("");
+    } else {
+      alert("Form cannot be submitted");
+    }
+  };
+
+  return (
+    <div className="form">
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          placeholder="Enter your Email"
+          onChange={handleEmailChange}
+          onFocus={handleInputFocus(setEmailBorderColor)}
+          onBlur={handleInputBlur(setEmailBorderColor, emailValid)}
+          onInput={handleInputChange(setEmailBorderColor)}
+          className={`input ${emailBorderColor}`}
+        />
+        {!emailValid && <div className="error">Invalid email format</div>}
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          placeholder="Enter your Password"
+          onChange={handlePasswordChange}
+          onFocus={handleInputFocus(setPasswordBorderColor)}
+          onBlur={handleInputBlur(setPasswordBorderColor, passwordValid)}
+          onInput={handleInputChange(setPasswordBorderColor)}
+          className={`input ${passwordBorderColor}`}
+        />
+        {!passwordValid && (
+          <div className="error">Password must be at least 8 characters</div>
+        )}
+
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          value={confirmPassword}
+          placeholder="Confirm your Password"
+          onChange={handleConfirmPasswordChange}
+          onFocus={handleInputFocus(setConfirmPasswordBorderColor)}
+          onBlur={handleInputBlur(
+            setConfirmPasswordBorderColor,
+            confirmPasswordValid
+          )}
+          onInput={handleInputChange(setConfirmPasswordBorderColor)}
+          className={`input ${confirmPasswordBorderColor}`}
+        />
+        {!confirmPasswordValid && (
+          <div className="error">Passwords do not match</div>
+        )}
+
+        <button type="button" onClick={handleSubmit}>
+          Sign Up
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
